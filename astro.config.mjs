c03e505a7a4cc342a20872a69c4643a3f10f4986
Okharev <florian.pellan@live.fr>
2025-01-07 22:26:42 +0100

    cloudflare pages migrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://pellanflorian.dev",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
