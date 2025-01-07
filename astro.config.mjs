1673f691e9e8df01d2f430090841374b804f75b1
Okharev <florian.pellan@live.fr>
2025-01-07 21:33:49 +0100

    pathing issue with search

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Okharev.github.io',
-  base: 'super-amazing-blog',
+  base: 'super-amazing-blog/',
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
     shikiConfig: {
